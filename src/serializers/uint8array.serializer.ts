688742df0eee19668d25746bfa567d44da14b5f2
Iuriiiiiii <alex.casas@webtroniclabs.com>
2024-11-19 20:48:30 -0300

    fix: versions and uint8array

--- src/serializers/uint8array.serializer.ts
+++ src/serializers/uint8array.serializer.ts
@@ -1,14 +1,20 @@
-import { NumberSerializationType } from "@online/tinyserializer/types";
+import {
+  NumberSerializationType,
+  type SerializeOptions,
+} from "@online/tinyserializer/types";
 import { mergeBuffers, numberSerializer } from "@online/tinyserializer";
 import { Opcode } from "../enums/mod.ts";
 
 export function uInt8ArraySerializer(
   value: unknown,
+  options: SerializeOptions,
 ): Uint8Array | null {
   if (!(value instanceof Uint8Array)) {
     return null;
   }
 
+  options.objectDatabase.getOrInsert(value);
+  
   const prefix = new Uint8Array([Opcode.Uint8Array]);
   const serializedLength = numberSerializer(
     value.length,
